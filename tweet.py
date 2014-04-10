94d4e05affe8b3b6738adbab09be9be401731e28
sakuramochi <sakuramochi.mochi.0@gmail.com>
2014-04-11 05:11:05 +0900

    integrate two credentials to one

--- tweet.py
+++ tweet.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
 from twython import Twython
-from twitter import oauth_dance
 import sys
+from os.path import basename
+
+cred_file = '.credentials'
 
 def auth():
     '''
     authonticated the account and return twitter class
     '''
     # read app credentials
-    app_creds = open('.app_credentials')
-    app_key = app_creds.readline().rstrip()
-    app_secret = app_creds.readline().rstrip()
-
-    # read oauth credentials
-    oauth_creds = open('.oauth_credentials')
-    if not oauth_creds:
-        oauth_dance("precure-today-cards", app_key, app_secret,
-                    '.oauth_credentials')
-    oauth_token = oauth_creds.readline().rstrip()
-    oauth_secret = oauth_creds.readline().rstrip()
-    return Twython(app_key, app_secret, oauth_token, oauth_secret)
+    with open(cred_file) as f:
+        app_key, app_secret, oauth_token, oauth_secret = \
+                            [x.strip() for x in f]
+    t = Twython(app_key, app_secret, oauth_token, oauth_secret)
+    return t
 
 def get_timeline(user_id='precure_cards'):
     '''
@@ -34,7 +29,6 @@ def get_timeline(user_id='precure_cards'):
 
 def tweet(status='', img_path=None):
     '''tweet a status text'''
-
     t = auth()
     if img_path:
         img = open(img_path, 'rb')
@@ -47,13 +41,15 @@ def tweet(status='', img_path=None):
 if __name__ == '__main__':
     args = sys.argv[1:]
     if len(args) < 2:
-        print('''Usage:
-    {0} <command> <arguments>
+        print('''\
+Usage:
+  {0} <command> <arguments>
 Example:
-    {0} tweet <args> -> tweet <args> text.
-    {0} img <arg1> <args> -> tweet <args> text with <arg1> image file.
-        '''.format(sys.argv[0]))
-    
+  {0} tweet <args>       tweet <args> text.
+  {0} img <img> <args>  tweet <args> text with <img> image file.'''.format(basename(sys.argv[0])))
+    elif sys.argv[1] == 'test':
+        cred_file = '.credentials_for_test'
+        sys.argv.pop(1)
     elif args[0] == 'tweet':
         tweet(status=args[1:])
     elif args[0] == 'img':
