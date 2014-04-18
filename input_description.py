30795643a9dd32e97dca29c632e1d7ee52f9b720
sakuramochi <sakuramochi.mochi.0@gmail.com>
2014-04-19 00:53:54 +0900

    change how to set variable

--- input_description.py
+++ input_description.py
@@ -9,22 +9,25 @@ db_file = 'cards.yaml'
 
 with open(db_file) as db:
     cards = yaml.load(db)
-for card in cards:
-    if str(card['chara_name']) == '': # if comment is empty
+cs = cards # avoid from RuntimeError: dictionary changed size during iteration
+for id, card in cs.items():
+    if not (('chara_name' in card.keys()) and ('card_name' in card.keys()) \
+            and ('comment_name' in card.keys()) and ('comment' in card.keys())):
         filename = card['filename']
         img = Image.open(img_dir + filename)
         img.show()
         msg = 'カードのデータを入力してね♪'
         title = 'カードデータの入力'
-        field_names = ['キャラクター', 'カード名', 'キャラクター', 'コメント']
+        field_names = ['キャラクター', 'なまえ', 'カード名', 'コメント']
         field_values = []
         field_values = eg.multenterbox(msg, title, field_names)
-        chara_name, card_name, comment_name, comment = field_values[:]
+        chara_name, comment_name, card_name, comment = field_values[:]
         if not chara_name:
             chara_name = 'リボン'
-        cards['chara_name'] = chara_name
-        cards['card_name'] = card_name
-        cards['comment_name'] = comment_name
-        cards['filename'] = filename
-        with open(db_file) as db:
+        cards[id]['chara_name'] = chara_name
+        cards[id]['card_name'] = card_name
+        cards[id]['comment_name'] = comment_name
+        cards[id]['comment'] = comment
+        print('write: {}'.format(field_values[:]))
+        with open(db_file, 'w') as db:
             yaml.dump(cards, db)
